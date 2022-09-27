2db66fd19ab37c861fe3454d96a2935bfaea0691
rrza69 <sr.ardhiyansah@gmail.com>
2022-09-27 13:59:32 +0700

    tugas pertemuan 3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Syahril Reza Ardiyansah
+			saya tinggal di rumah
+			olahraga yang saya sukai adalah tidur";
+	}
+} 
